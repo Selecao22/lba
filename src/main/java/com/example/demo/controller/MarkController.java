78711873b2f1aee0e15250c81f19f48c56d63a64
Nikita Yazikov <cour1337@gmail.com>
2020-03-19 12:52:48 +0300

    lab 4 is completed

--- src/main/java/com/example/demo/controller/MarkController.java
+++ src/main/java/com/example/demo/controller/MarkController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.MarkJdbc;
 import com.example.demo.model.Mark;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MarkController
 {
     private final MarkJdbc markJdbc;
